3fcdaa5319da6b1aaaf392541e4d37efb6fa259d
marselich <marsellkaliev7722@gmail.com>
2022-07-01 16:13:18 +0500

    finished with instanceof

--- src/ru/sinergy/Main.java
+++ src/ru/sinergy/Main.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Object o = new Scanner(System.in);
-        Scanner scanner = null;
-        if(o instanceof Scanner) // проверка на тип данных
-            scanner = (Scanner) o;
+//        Object o = new Scanner(System.in);
+//        Scanner scanner = null;
+//        if(o instanceof Scanner) // проверка на тип данных
+//            scanner = (Scanner) o;
+//
+//        if(scanner != null) {
+//            scanner.nextInt();
+//        }
+//
+//        Object o1 = new String();
+//        Object o2 = new Integer(14);
 
-        if(scanner != null) {
-            scanner.nextInt();
+    Object[] objects = {10, "Привет", 3.14}; //здесь происходит автоупаковка
+
+    for(Object o: objects) {
+        if(o instanceof String) {
+            String s = (String) o;
+            System.out.println(s);
         }
-
-        Object o1 = new String();
-        Object o2 = new Integer(14);
-
+    }
 
 
     }
