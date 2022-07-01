234927d60516edb0f67e069ee69e0e24041a9617
marselich <marsellkaliev7722@gmail.com>
2022-07-01 16:40:00 +0500

    example of not working object cast

---
+++ src/ru/sinergy/customgenerictypes/Account.java
@@ -0,0 +1,27 @@
+package ru.sinergy.customgenerictypes;
+
+public class Account {
+    private Object id;
+    private int sum;
+
+    public Account(Object id, int sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public Object getId() {
+        return id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setId(Object id) {
+        this.id = id;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+}
--- src/ru/sinergy/customgenerictypes/Main.java
+++ src/ru/sinergy/customgenerictypes/Main.java
@@ -1,7 +1,15 @@
 package ru.sinergy.customgenerictypes;
 
 public class Main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
+        Account acc1 = new Account(123123, 511244);
+        System.out.println(acc1.getId());
 
+
+        Account acc2 = new Account("gdsf324gsd", 511244);
+        System.out.println(acc2.getId());
+
+        int a = ((Integer)acc2.getId()) + 10;
+        System.out.println(a);
     }
 }
